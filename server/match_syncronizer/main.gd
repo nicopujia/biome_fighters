0dc668b1ca02110da7057718696eec7cae1f67ba
Nico Pujia <nicobatman0@gmail.com>
2023-12-02 22:22:20 -0300

    Fix prints, players numbers and IP address

--- server/match_syncronizer/main.gd
+++ server/match_syncronizer/main.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-const REPLICATION_PROPERTIES = [
+const REPLICATION_PROPERTIES: PackedStringArray = [
 	"Player:position",
 	"AnimatedSprite2D:animation",
 	"AnimatedSprite2D:frame",
@@ -19,19 +19,20 @@ func _ready() -> void:
 	for property in REPLICATION_PROPERTIES:
 		replication_config.add_property(property)
 	
-	var multiplayer_peer = ENetMultiplayerPeer.new()
+	var multiplayer_peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+	multiplayer_peer.set_bind_ip("0.0.0.0")
 	var error: Error = multiplayer_peer.create_server(port, 2)
 	multiplayer_peer.peer_connected.connect(_on_peer_connected)
 	multiplayer_peer.peer_disconnected.connect(_on_peer_disconnected)
 	multiplayer.multiplayer_peer = multiplayer_peer
-	print("Server created at port %s with error %s" % [port, error])
+	printraw("Server created at port %s with error %s" % [port, error], "\n")
 
 
 func _get_argurments() -> void:
 	var arguments: Dictionary = {}
 	for argument in OS.get_cmdline_args():
 		if argument.find("=") > -1:
-			var key_value = argument.split("=")
+			var key_value: PackedStringArray = argument.split("=")
 			arguments[key_value[0].lstrip("--")] = key_value[1]
 		else:
 			arguments[argument.lstrip("--")] = ""
@@ -40,27 +41,27 @@ func _get_argurments() -> void:
 
 
 func _on_peer_connected(new_peer_id : int) -> void:
-	print("Peer %s is joining..." % new_peer_id)
+	printraw("Peer %s is joining..." % new_peer_id, "\n")
 	await get_tree().create_timer(1).timeout
 	connected_peer_ids.append(new_peer_id)
-	print("Peer %s joined" % new_peer_id)
-	print("Currently connected peers: ", connected_peer_ids)
+	printraw("Peer %s joined" % new_peer_id, "\n")
+	printraw("Currently connected peers: ", connected_peer_ids, "\n")
 	
 	if len(connected_peer_ids) == 2:
-		print("Both peers have joined. Starting the game...")
+		printraw("Both peers have joined. Starting the game...", "\n")
 		rpc("_start_game", connected_peer_ids)
 
 
 func _on_peer_disconnected(id: int) -> void:
 	connected_peer_ids.remove_at(connected_peer_ids.find(id))
-	print("Peer %s disconnected" % id)
-	print("Currently connected peers: ", connected_peer_ids)
+	printraw("Peer %s disconnected" % id, "\n")
+	printraw("Currently connected peers: ", connected_peer_ids, "\n")
 
 
 @rpc("reliable")
 func _start_game(peer_ids: PackedInt32Array):
 	for i in len(peer_ids):
-		get_node("Battle/Map/SpawnPoints/Player" + str(i)).add_child(_create_player(peer_ids[i]))
+		get_node("Battle/Map/SpawnPoints/Player" + str(i + 1)).add_child(_create_player(peer_ids[i]))
 
 
 func _create_player(id: int) -> Node:
